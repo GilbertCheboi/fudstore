e90680d1018f0176193da2ac18f476dfe97edc5c
Gilbert <gilbertcheboi100@gmail.com>
2018-08-14 13:30:58 +0300

    try

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -199,9 +199,9 @@ SECURE_FRAME_DENY               = False
 
 AWS_STORAGE_BUCKET_NAME = 'ecommerce-foodstore'
 AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = 'AKIAIWUCCK4CVBIFLNGA'
+#AWS_ACCESS_KEY_ID = 'AKIAJ2RXRPFZGOIDEMAA'
 
-AWS_SECRET_ACCESS_KEY = 'q/ELKmZLaA01j14JG2z0tHWFEDCMXVnrq0faBXBh'
+#AWS_SECRET_ACCESS_KEY = 'hYOdXGwf7WW0EcTymlw34xhmEw7maXHZvVDfdCMG'
 
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -193,4 +193,28 @@ SECURE_FRAME_DENY               = False
 
 
 
+AWS_STORAGE_BUCKET_NAME = 'ecommerce-foodstore'
+AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
+#AWS_ACCESS_KEY_ID = 'AKIAJ2RXRPFZGOIDEMAA'
+
+#AWS_SECRET_ACCESS_KEY = 'hYOdXGwf7WW0EcTymlw34xhmEw7maXHZvVDfdCMG'
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+
+
+
+
+
+
 
