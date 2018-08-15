2f0c35ca66479aa3c886d368955a7b8855e78f50
Gilbert <gilbertcheboi100@gmail.com>
2018-08-15 11:23:19 +0300

    try

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '@!-)jwmuzh8btr380g61=g+#&zzei&dz2(&=xbvxztady)_p(r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['fudstore-env.ckuzj6mhuz.us-east-1.elasticbeanstalk.com','fudstore-env.ckuzj6mhuz.us-east-1.elasticbeanstalk.com']
 #ALLOWED_HOSTS = []
