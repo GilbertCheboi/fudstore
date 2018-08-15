60048b8f5f75860f6bb47a4517ca475cf11de3ee
Gilbert <gilbertcheboi100@gmail.com>
2018-08-15 13:42:18 +0300

    try

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = [ 'fudstore-env.ckuzj6mhuz.us-east-1.elasticbeanstalk.com']
 
 EMAIL_HOST = 'amzon.gmail.com'
 EMAIL_HOST_USER = 'gilbertcheboi100@gmail.com' # sendgrid
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'yourpassword')
+EMAIL_HOST_PASSWORD =  '30230192g'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Python ecommerce <gilbertcheboi100@gmail.com>'
@@ -194,3 +194,20 @@ SECURE_FRAME_DENY               = False
 
 
 
+AWS_STORAGE_BUCKET_NAME = 'bucket-foodstore'
+AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID =  ' '
+
+AWS_SECRET_ACCESS_KEY =  ' '
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
